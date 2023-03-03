a4487a8ec2a56a4d65b91f67aee382168f72b3b7
Hendruch <hendruchrasgado@gmail.com>
2023-03-03 16:48:31 -0600

    login y registro empresa

---
+++ lib/screens/registro_Empresa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistroEmpresaScreen extends StatelessWidget {
+   
+  const RegistroEmpresaScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('RegistroEmpresaScreen'),
+      ),
+    );
+  }
+}
