549f45f1b4abf6aaafae10594935347ba0421a6b
Hendruch <hendruchrasgado@gmail.com>
2023-03-03 16:06:00 -0600

    build update

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(appBar: AppBar(title: const Text("Hola"),)),
+      home: Scaffold(appBar: AppBar(title: const Text("Hola Luisfe"),)),
     );
   }
 }
