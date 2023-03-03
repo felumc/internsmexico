66c1cc2de831a8cf20e73592bf739c37cd0a7ff2
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-03-03 17:31:33 -0600

    "listas"

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,80 @@
 import 'package:flutter/material.dart';
+import '/screens/login.dart';
+import '/screens/registro_Empresa.dart';
+import '/screens/registro_Estudiante.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Interns México',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(appBar: AppBar(title: const Text("Hola Luisfe"),)),
+      home: const MyHomePage(title: 'List of Pages'),
+      routes: {
+        '/Login': (_) => const LoginScreen(),
+        '/Registro_de_estudiantes': (_) => const RegistroEstudianteScreen(),
+        '/Registro_de_empresas': (_) => const RegistroEmpresaScreen(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          String itemName = '';
+          switch (index) {
+            case 0:
+              itemName = 'Login Screen';
+              break;
+            case 1:
+              itemName = 'Registro de Estudiantes Screen';
+              break;
+            case 2:
+              itemName = 'Registro de Empresas Screen';
+              break;
+            default:
+              itemName = 'Unknown';
+              break;
+          }
+          return ListTile(
+            title: Text(itemName),
+            onTap: () {
+              switch (index) {
+                case 0:
+                  Navigator.of(context).pushNamed('/Login');
+                  break;
+                case 1:
+                  Navigator.of(context).pushNamed('/Registro_de_estudiantes');
+                  break;
+                case 2:
+                  Navigator.of(context).pushNamed('/Registro_de_empresas');
+                  break;
+                default:
+              }
+            },
+          );
+        },
+        itemCount: 3,
+      ),
     );
   }
 }
