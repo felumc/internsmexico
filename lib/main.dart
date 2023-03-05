4e0445373c16101be76449b1d853bb9d500f9922
EliteHD <culeleplebi@gmail.com>
2023-03-04 22:02:00 -0600

    V 0.2.1 login

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'V 0.1.0 Registro Empresas Agregado'),
+      home: const MyHomePage(title: 'V 0.2.0 Pre-login Agregado'),
       routes: {
         '/Login': (_) => const LoginScreen(),
         '/Registro_de_estudiantes': (_) => const RegistroEstudianteScreen(),
